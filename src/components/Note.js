96f41be1cb192550d95df0c2722d148d4ab2318e
annastepanova <annastepanova1811@gmail.com>
2020-01-25 20:00:58 -0500

    update state with current note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import NotesContext from '../context'
 
 export default function Note({note}) {
+  const {dispatch} = useContext(NotesContext)
   return (
     <div className="note">
       <p>{note.text}</p>
       <div className="btn-container">
-        <button className="edit">Edit</button>
+        <button onClick={() => dispatch({type: 'SET_CURRENT_NOTE', payload: note})} className="edit">Edit</button>
         <button className="delete">Delete</button>
       </div>
     </div>
   )
-
 }
