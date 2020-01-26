d14e2df7efd2e19cecd29e11f53589958b6f3be4
annastepanova <annastepanova1811@gmail.com>
2020-01-26 00:15:37 -0500

    AddNote function component

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState, useContext, useEffect, useRef} from 'react'
+import NotesContext from '../context'
 
-export default function() {
+export default function AddNote() {
+  const {state, dispatch} = useContext(NotesContext)
+  const [value, setValue] = useState('')
+  let ref = useRef()
+  useEffect(() => {
+    ref.current.focus()
+  }, [])
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (value.trim() === '') {
+      alert('Cannot add blank note')
+    }
+    else {
+      dispatch({type: 'ADD_NOTE', payload: value})
+      setValue('')
+    }
+  }
+
   return (
     <div className="note-form">
-      <form action="">
-        <input type="text"/>
+      <form onSubmit={handleSubmit} action="">
+        <input type="text" ref={ref} onChange={handleChange} value={value} />
         <button>Add Note</button>
       </form>
     </div>
